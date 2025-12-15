4a2ade77105cd49dabdc400b7907d3c26ddf8462
ParthAgarwal-05 <parth051006@gmail.com>
2025-12-15 14:46:22 +0530

    added the .coords command

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mineflayer = require('mineflayer');
 // Create the bot instance
 const bot = mineflayer.createBot({
   host: process.env.MC_HOST || "localhost",     // Server IP
-  port: process.env.MC_PORT ? parseInt(process.env.MC_PORT) : 25565,
+  port: process.env.MC_PORT ? parseInt(process.env.MC_PORT) : 425565,
   username: process.env.MC_USERNAME || "PatchFestBot" // Bot username
 });
 
@@ -19,9 +19,14 @@ bot.once("spawn", () => {
 // Basic chat command listener
 bot.on("chat", (username, message) => {
 
-  if (username === bot.username) return; // ignore itself
+  if (username === bot.username) return;
 
-  if (message === ".hello") {
+  if (message.toLowerCase().startsWith(".hello")) {
     bot.chat(`Hello ${username}! I am your helper bot 🤝`);
+  }
+  
+  if (message.startsWith(".coords")) {
+      const p = bot.entity.position;
+      bot.chat(`I am at ${p.x.toFixed(0)}, ${p.y.toFixed(0)}, ${p.z.toFixed(0)}`);
   }
 });
